d8ca1a56ea1073698ae9d1ecbe442fd6125b2717
Necralma <lihuel.almada@gmail.com>
2020-06-19 10:02:16 -0300

    add upgrates

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 (function(){
     'use strict';
     window.addEventListener('load',init,false);
@@ -14,48 +13,53 @@
     var pause=true;
     var gameover=true;
     var score=0;
-    var player=new Rectangle(90,280,10,10,3);
+    var multishot=1;
+    var player=new Rectangle(90,280,10,10,0,3);
     var shots=[];
-    var enemies=[];
-    function random(max){
-    return ~~(Math.random()*max);
-    }
-    function init(){
-    canvas=document.getElementById('canvas');
-    ctx=canvas.getContext('2d');
-    canvas.width=200;
-    canvas.height=300;
-    run();
-    repaint();
-    }
-    function run(){
-    setTimeout(run,50);
-    act();
-    }
-    function repaint(){
-    requestAnimationFrame(repaint);
-    paint(ctx);
-    }
-    function reset(){
-    score=0;
-    player.x=90;
-    player.y=280;
-    player.health=3;
-    player.timer=0;
-    shots.length=0;
-    enemies.length=0;
-    enemies.push(new Rectangle(10,0,10,10,2));
-    gameover=false;
-    }
-    function act(){
-    if(!pause){
-    // GameOver Reset
-    if(gameover)
-    reset();
-    // Move Player
-    //if(pressing[KEY_UP])
-    // player.y-=10;
-    
+var enemies=[];
+var powerups=[];
+var messages=[];
+function random(max){
+return ~~(Math.random()*max);
+}
+function init(){
+canvas=document.getElementById('canvas');
+ctx=canvas.getContext('2d');
+canvas.width=200;
+canvas.height=300;
+run();
+repaint();
+}
+function run(){
+setTimeout(run,50);
+act();
+}
+function repaint(){
+requestAnimationFrame(repaint);
+paint(ctx);
+}
+function reset(){
+score=0;
+multishot=1;
+player.x=90;
+player.y=280;
+player.health=3;
+player.timer=0;
+shots.length=0;
+enemies.length=0;
+powerups.length=0;
+messages.length=0;
+enemies.push(new Rectangle(10,0,10,10,0,2));
+gameover=false;
+}
+function act(){
+if(!pause){
+// GameOver Reset
+if(gameover)
+reset();
+// Move Player
+//if(pressing[KEY_UP])
+// player.y-=10;
 if(pressing[KEY_RIGHT])
 player.x+=10;
 //if(pressing[KEY_DOWN])
@@ -69,6 +73,16 @@ if(player.x<0)
 player.x=0;
 // New Shot
 if(lastPress==KEY_SPACE){
+if(multishot==3){
+shots.push(new Rectangle(player.x-3,player.y+2,5,5));
+shots.push(new Rectangle(player.x+3,player.y,5,5));
+shots.push(new Rectangle(player.x+9,player.y+2,5,5));
+}
+else if(multishot==2){
+shots.push(new Rectangle(player.x,player.y,5,5));
+shots.push(new Rectangle(player.x+5,player.y,5,5));
+}
+else
 shots.push(new Rectangle(player.x+3,player.y,5,5));
 lastPress=null;
 }
@@ -77,6 +91,43 @@ for(var i=0,l=shots.length;i<l;i++){
 shots[i].y-=10;
 if(shots[i].y<0){
 shots.splice(i--,1);
+l--;
+}
+}
+// Move Messages
+for(var i=0,l=messages.length;i<l;i++){
+messages[i].y+=2;
+if(messages[i].y<260){
+messages.splice(i--,1);
+l--;
+}
+}
+// Move PowerUps
+for(var i=0,l=powerups.length;i<l;i++){
+powerups[i].y+=10;
+// Powerup Outside Screen
+if(powerups[i].y>canvas.height){
+powerups.splice(i--,1);
+l--;
+continue;
+}
+// Player intersects
+if(player.intersects(powerups[i])){
+if(powerups[i].type==1){ // MultiShot
+if(multishot<3){
+multishot++;
+messages.push(new Message('MULTI',player.x,player.y));
+}
+else{
+score+=5;
+messages.push(new Message('+5',player.x,player.y));
+}
+}
+else{ // ExtraPoints
+score+=5;
+messages.push(new Message('+5',player.x,player.y));
+}
+powerups.splice(i--,1);
 l--;
 }
 }
@@ -93,7 +144,7 @@ if(enemies[i].health<1){
 enemies[i].x=random(canvas.width/10)*10;
 enemies[i].y=0;
 enemies[i].health=2;
-enemies.push(new Rectangle(random(canvas.width/10)*10,0,10,10,2));
+enemies.push(new Rectangle(random(canvas.width/10)*10,0,10,10,0,2));
 }
 else{
 enemies[i].timer=1;
@@ -103,6 +154,7 @@ ll--;
 }
 }
 enemies[i].y+=10;
+// Enemy Outside Screen
 if(enemies[i].y>canvas.height){
 enemies[i].x=random(canvas.width/10)*10;
 enemies[i].y=0;
@@ -119,10 +171,18 @@ if(shots[j].intersects(enemies[i])){
 score++;
 enemies[i].health--;
 if(enemies[i].health<1){
+// Add PowerUp
+var r=random(20);
+if(r<5){
+if(r==0) // New MultiShot
+powerups.push(new Rectangle(enemies[i].x,enemies[i].y,10,10,1));
+else // New ExtraPoints
+powerups.push(new Rectangle(enemies[i].x,enemies[i].y,10,10,0));
+}
 enemies[i].x=random(canvas.width/10)*10;
 enemies[i].y=0;
 enemies[i].health=2;
-enemies.push(new Rectangle(random(canvas.width/10)*10,0,10,10,2));
+enemies.push(new Rectangle(random(canvas.width/10)*10,0,10,10,0,2));
 }
 else{
 enemies[i].timer=1;
@@ -153,6 +213,13 @@ ctx.fillRect(0,0,canvas.width,canvas.height);
 ctx.fillStyle='#0f0';
 if(player.timer%2==0)
 player.fill(ctx);
+for(var i=0,l=powerups.length;i<l;i++){
+if(powerups[i].type==1)
+ctx.fillStyle='#f90';
+else
+ctx.fillStyle='#cc6';
+powerups[i].fill(ctx);
+}
 for(var i=0,l=enemies.length;i<l;i++){
 if(enemies[i].timer%2==0)
 ctx.fillStyle='#00f';
@@ -164,6 +231,8 @@ ctx.fillStyle='#f00';
 for(var i=0,l=shots.length;i<l;i++)
 shots[i].fill(ctx);
 ctx.fillStyle='#fff';
+for(var i=0,l=messages.length;i<l;i++)
+ctx.fillText(messages[i].string,messages[i].x,messages[i].y);
 ctx.fillText('Score: '+score,0,20);
 ctx.fillText('Health: '+player.health,150,20);
 //ctx.fillText('Last Press: '+lastPress,0,20);
@@ -184,11 +253,12 @@ pressing[evt.keyCode]=true;
 document.addEventListener('keyup',function(evt){
 pressing[evt.keyCode]=false;
 },false);
-function Rectangle(x,y,width,height,health){
+function Rectangle(x,y,width,height,type,health){
 this.x=(x==null)?0:x;
 this.y=(y==null)?0:y;
 this.width=(width==null)?0:width;
 this.height=(height==null)?this.width:height;
+this.type=(type==null)?1:type;
 this.health=(health==null)?1:health;
 this.timer=0;
 }
@@ -202,6 +272,11 @@ this.y+this.height>rect.y);
 }
 Rectangle.prototype.fill=function(ctx){
 ctx.fillRect(this.x,this.y,this.width,this.height);
+}
+function Message(string,x,y){
+this.string=(string==null)?'?':string;
+this.x=(x==null)?0:x;
+this.y=(y==null)?0:y;
 }
 window.requestAnimationFrame=(function(){
 return window.requestAnimationFrame ||
